e3c45daee948e7e06a5c7d0860d93b57fadd74c6
Mat Booth <mat.booth@gmail.com>
2023-02-24 11:31:30 +0000

    Scaling test improvements
    * Show both interpolation methods
    * Add a 2x-only case

--- ScalingTest/src/uk/co/matbooth/ScalingTest.java
+++ ScalingTest/src/uk/co/matbooth/ScalingTest.java
@@ -1,6 +1,6 @@
 package uk.co.matbooth;
 
-import java.net.URISyntaxException;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +8,8 @@ import java.util.Objects;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.ImageDataProvider;
@@ -21,50 +23,79 @@ import org.eclipse.swt.widgets.Shell;
 
 public class ScalingTest {
 
-	private final List<String> imageNames = List.of("gear_icon0", /*"gear_icon1", "gear_icon2",*/ "gear_icon3");
+	private final List<String> imageNames = List.of("gear_icon0", "gear_icon1", "gear_icon2");
 	private final List<Image> images = new ArrayList<>();
 
-	private void createSampleRow(Composite parent, String imageName) throws URISyntaxException {
-		URL resource100 = getClass().getResource(imageName + ".png");
-//		URL resource150 = getClass().getResource(imageName + "@1.5x.png");
-		URL resource200 = getClass().getResource(imageName + "@2x.png");
+	private static void fixupAutoScaleMethod(boolean nearest) {
+		try {
+			Field autoScaleMethod = DPIUtil.class.getDeclaredField("autoScaleMethod");
+			autoScaleMethod.setAccessible(true);
+			if (nearest) {
+				Enum value = Enum.valueOf((Class<Enum>) autoScaleMethod.getType(), "NEAREST");
+				autoScaleMethod.set(null, value);
+			} else {
+				Enum value = Enum.valueOf((Class<Enum>) autoScaleMethod.getType(), "SMOOTH");
+				autoScaleMethod.set(null, value);
+			}
+		} catch (ReflectiveOperationException | RuntimeException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void createSampleRow(Composite parent, String imageName) throws Exception {
 
 		StringBuilder available = new StringBuilder();
-		if(!Objects.isNull(resource100)) {
+		if (!Objects.isNull(getClass().getResource(imageName + ".png"))) {
 			available.append("1x");
 		}
-//		if(!Objects.isNull(resource150)) {
-//			available.append(", 1.5x");
-//		}
-		if(!Objects.isNull(resource200)) {
-			available.append(", 2x");
+		if (!Objects.isNull(getClass().getResource(imageName + "@2x.png"))) {
+			if (available.length() != 0) {
+				available.append(", ");
+			}
+			available.append("2x");
 		}
-
-		ImageDescriptor desc = ImageDescriptor.createFromURL(resource100);
-		Image image = desc.createImage();
-
-		boolean[] found = new boolean[2];
-		ImageData choiceData = DPIUtil.validateAndGetImageDataAtZoom((ImageDataProvider) desc, DPIUtil.getDeviceZoom(),
-				found);
 
 		Label label = new Label(parent, SWT.NONE);
 		label.setText(available.toString());
-		Label pic = new Label(parent, SWT.NONE);
-		pic.setImage(image);
+
+		URL resource = getClass().getResource(".").toURI().resolve(imageName + ".png").toURL();
+
+		fixupAutoScaleMethod(true);
+		ImageDescriptor descNearest = ImageDescriptor.createFromURL(resource);
+		Image imageNearest = descNearest.createImage();
+
+		Label picNearest = new Label(parent, SWT.NONE);
+		picNearest.setImage(imageNearest);
+		picNearest.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
+
+		fixupAutoScaleMethod(false);
+		ImageDescriptor descSmooth = ImageDescriptor.createFromURL(resource);
+		Image imageSmooth = descSmooth.createImage();
+
+		Label picSmooth = new Label(parent, SWT.NONE);
+		picSmooth.setImage(imageSmooth);
+		picSmooth.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
+
 		Label labelChoice = new Label(parent, SWT.NONE);
-		switch (choiceData.width) {
-		case 16:
-			labelChoice.setText("1x (" + choiceData.width + "," + choiceData.height + ")");
-			break;
-//		case 24:
-//			labelChoice.setText("1.5x (" + choiceData.width + "," + choiceData.height + ")");
-//			break;
-		case 32:
-			labelChoice.setText("2x (" + choiceData.width + "," + choiceData.height + ")");
-			break;
+		boolean[] found = new boolean[2];
+		try {
+			ImageData choiceData = DPIUtil.validateAndGetImageDataAtZoom((ImageDataProvider) descNearest,
+					DPIUtil.getDeviceZoom(), found);
+			switch (choiceData.width) {
+			case 32:
+				labelChoice.setText("1x (" + choiceData.width + "," + choiceData.height + ")");
+				break;
+			case 64:
+				labelChoice.setText("2x (" + choiceData.width + "," + choiceData.height + ")");
+				break;
+			}
+		} catch (Exception e) {
+			labelChoice.setText("Error");
+			e.printStackTrace();
 		}
 
-		images.add(image);
+		images.add(imageNearest);
+		images.add(imageSmooth);
 	}
 
 	public void run() throws Exception {
@@ -77,21 +108,38 @@ public class ScalingTest {
 		zoomLabel.setText("Current Scaling Factor: " + DPIUtil.getDeviceZoom() / 100f + "x");
 
 		Composite comp = new Composite(shell, SWT.NONE);
-		comp.setLayout(new GridLayout(3, false));
+		comp.setLayout(new GridLayout(4, false));
 
 		Label availableHeading = new Label(comp, SWT.NONE);
 		availableHeading.setText("Available:");
+
 		Label choiceHeading = new Label(comp, SWT.NONE);
 		choiceHeading.setText("Chosen:");
 		GridData gridData = new GridData();
-		gridData.horizontalSpan = 2;
+		gridData.horizontalSpan = 3;
 		choiceHeading.setLayoutData(gridData);
+
+		new Label(comp, SWT.NONE);
+		Label nHeading = new Label(comp, SWT.NONE);
+		nHeading.setText("Nearest");
+		FontData[] nFontData = nHeading.getFont().getFontData();
+		nFontData[0].setHeight(5);
+		nHeading.setFont(new Font(display, nFontData));
+		nHeading.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
+		Label sHeading = new Label(comp, SWT.NONE);
+		sHeading.setText("Smooth");
+		FontData[] sFontData = sHeading.getFont().getFontData();
+		sFontData[0].setHeight(5);
+		sHeading.setFont(new Font(display, sFontData));
+		sHeading.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
+		new Label(comp, SWT.NONE);
 
 		for (String imageName : imageNames) {
 			createSampleRow(comp, imageName);
 		}
 
 		shell.open();
+		shell.pack();
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
